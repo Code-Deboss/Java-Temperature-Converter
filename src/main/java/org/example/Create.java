8e8d2442f17eb4da0b89a52602af0dcca99bc52d
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-03-16 21:13:32 +0100

    Create class is responsible for getting the temperature value and conversion choice from the user :)

--- src/main/java/org/example/Create.java
+++ src/main/java/org/example/Create.java
@@ -9,5 +9,13 @@ public class Create {
         double temperatureValue = input.nextDouble();
         return temperatureValue;
     }
-
+    public static int getConversionChoice() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Which conversion would you like to do?");
+        System.out.println("1. Fahrenheit to Celsius");
+        System.out.println("2. Celsius to Fahrenheit");
+        System.out.print("Enter your choice (1 or 2): ");
+        int choice = input.nextInt();
+        return choice;
+    }
 }
