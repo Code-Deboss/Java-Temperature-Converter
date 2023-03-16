2bbcfbbe6b3e202bba6634806b4398aacaa3a2a8
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-03-16 21:16:24 +0100

    Update class contains the temperature conversion logic, with methods for converting from Fahrenheit to Celsius

---
+++ src/main/java/org/example/Update.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Update {
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        double celsius = (fahrenheit - 32) * 5/9;
+        return celsius;
+    }
+}
