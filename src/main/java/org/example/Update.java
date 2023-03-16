bcab7469c89ac8c793c08a809c5e2d167da72a11
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-03-16 21:17:00 +0100

    converts from Celsius to Fahrenheit.

--- src/main/java/org/example/Update.java
+++ src/main/java/org/example/Update.java
@@ -5,4 +5,8 @@ public class Update {
         double celsius = (fahrenheit - 32) * 5/9;
         return celsius;
     }
+    public static double celsiusToFahrenheit(double celsius) {
+        double fahrenheit = (celsius * 9/5) + 32;
+        return fahrenheit;
+    }
 }
